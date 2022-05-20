daa8f8175bdf33b0654df2232ba93e69bb00e3bc
Nehal <zaidprismitsystems@gmail.com>
2022-05-20 11:51:07 +0530

    change in app.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  const myName = "Rakesh";
+  return (
+    <div className="App">
+    hello
+      Hello My Name is ${myName}
+    </div>
+  );
+}
+
+export default App;
